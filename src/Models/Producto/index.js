69c5a79c7b47d3baa4d89223b3feeadaaf2ba4b1
Mauricio <mcampo26@gmail.com>
2025-08-20 22:56:19 -0300

    Fix: case-sensitive Ingrediente.js y re-exports

--- src/Models/Producto/index.js
+++ src/Models/Producto/index.js
@@ -1,4 +1,3 @@
-// src/Models/Producto/index.js
 export { default as Producto }     from './producto.js';
 export { default as TipoProducto } from './tipoProducto.js';
-export { default as Ingrediente }  from './Ingrediente.js'; // I mayúscula
+export { default as Ingrediente }  from './Ingrediente.js';
